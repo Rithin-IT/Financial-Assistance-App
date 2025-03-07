f40831269946bbe07ec94c9090509aeb892093e5
josphin89 <josejosphin89@gmail.com>
2025-03-07 16:38:41 +0530

    register_jose_final

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 
 
 function Register() {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const registerGlobalState = useSelector((state) => state.register.registerList)
@@ -20,61 +20,81 @@ function Register() {
         password: ""
     })
     const userRegister = () => {
-        const formData = new FormData();
-        formData.append("name",inputValue.name);
-        formData.append("email",inputValue.email);
-        formData.append("password",inputValue.password);
 
-        axios.post('https://agaram.academy/api/b4/action.php?request=ai_finance_user_register',formData).then((res)=>{
-            // console.log(res)
-            alert("Successfully Registered");
-            navigate('/login');
-            
-        })
-     
-    }
-    return <div style={
-        {
-            backgroundImage:`url(${web6})`,
-            backgroundSize:"cover",
-            backgroundRepeat:"no-repeat",
-            height:"750px"
-        }
-    }>
-    
-    
-        <h1 style={{ textAlign:"center"
-    
-        }}>Register</h1><br></br>
-        <div style={{ backgroundColor: "wheat", width: "500px", marginLeft: "350px", borderRadius: "15px" ,marginLeft:"500px", background:"transparent", boxShadow: "0 0 10px"}}>
-            <Form style={{ padding: "45px" }}>
-                <Form.Label>Name</Form.Label>
-                <Form.Control style={{ marginTop: "10px" }} value={inputValue.name} type="text" placeholder="Enter your name" onChange={(e) => setInputValue({ ...inputValue, name: e.target.value })} required  ></Form.Control><br></br>
-                <Form.Label>Email</Form.Label>
-                <Form.Control style={{ marginTop: "10px" }}  value={inputValue.email} type="email" placeholder="Enter your email" onChange={(e) => setInputValue({ ...inputValue, email: e.target.value })} required  ></Form.Control><br></br>
-                <Form.Label>password</Form.Label>
-                <Form.Control style={{ marginTop: "10px" }} value={inputValue.password} type="password" placeholder="Enter your password" onChange={(e) => setInputValue({ ...inputValue, password: e.target.value })} required  ></Form.Control><br></br>
-                <br></br>
-                <Button variant="dark" style={{marginLeft:"150px"}} onClick={userRegister}>Register</Button>
-            </Form>
-        </div>
-    
-    </div>
-    }
-    export default Register
-        
-        
        
+        const formData = new FormData();
+        formData.append("name", inputValue.name);
+        formData.append("email", inputValue.email);
+        formData.append("password", inputValue.password);
 
+        axios.post('https://agaram.academy/api/b4/action.php?request=ai_finance_user_register', formData)
+            .then((res) => {
+                // console.log(res)
+                alert("Successfully Registered");
+                navigate('/login');
+            })
+            if(!inputValue.name.trim()|| 
+                !inputValue.email().trim()||
+                !inputValue.password.trim()){
+                    alert("Please fill the input value")
+                }
 
-
-
-      
-
-               
-
-
-                
+        else if(inputValue.name && inputValue.email && inputValue.password){ 
+           alert("Register Success")
+        setUser({name:"",email:"",password:""})
+         navigate('/login')
+    }
+    else{
+        alert("please fillup")
+    }
         
 
-    
+}
+
+
+return <div style={
+    {
+        backgroundImage: `url(${web6})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "750px"
+    }
+}>
+
+
+    <h1 style={{
+        textAlign: "center"
+
+    }}>Register</h1><br></br>
+    <div style={{ backgroundColor: "wheat", width: "500px", marginLeft: "350px", borderRadius: "15px", marginLeft: "500px", background: "transparent", boxShadow: "0 0 10px" }}>
+        <Form style={{ padding: "45px" }}>
+            <Form.Label>Name</Form.Label>
+            <Form.Control style={{ marginTop: "10px" }} value={inputValue.name} type="text" placeholder="Enter your name" onChange={(e) => setInputValue({ ...inputValue, name: e.target.value })} required  ></Form.Control><br></br>
+            <Form.Label>Email</Form.Label>
+            <Form.Control style={{ marginTop: "10px" }} value={inputValue.email} type="email" placeholder="Enter your email" onChange={(e) => setInputValue({ ...inputValue, email: e.target.value })} required  ></Form.Control><br></br>
+            <Form.Label>password</Form.Label>
+            <Form.Control style={{ marginTop: "10px" }} value={inputValue.password} type="password" placeholder="Enter your password" onChange={(e) => setInputValue({ ...inputValue, password: e.target.value })} required  ></Form.Control><br></br>
+            <br></br>
+            <Button variant="dark" style={{ marginLeft: "150px" }} onClick={userRegister}>Register</Button>
+        </Form>
+    </div>
+
+</div>
+    }
+export default Register
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
