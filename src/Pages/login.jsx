6f750d4c9e9807e08658061ffb22147eb361b93b
Manju358 <manjurajkumar98@gmail.com>
2025-03-07 15:32:28 +0530

    login final march-7

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,10 +1,72 @@
 
 
-function Login(){
+import { useState, useEffect } from 'react';
 
-    return <div>
-            <h1>Login</h1>
-            </div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, Table } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+
+import { setLoginUsers } from '../Redux/Slices/login';
+
+
+
+function Login() {
+
+  
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [loginDetails, setLoginDetails] = useState({
+        email: "",
+        password: ""
+    })
+   
+    const submit = () => {
+                
+        if (loginDetails.email === "" || loginDetails.password === "") {
+            alert("Please Fill");
+            return;
+        }
+        
+        const formData = new FormData();
+        formData.append("email", loginDetails.email);
+        formData.append("password", loginDetails.password);
+
+        axios.post("https://agaram.academy/api/b4/action.php?request=ai_finance_user_login", formData)
+            .then((response) => {
+                
+                console.log(response);
+                
+               
+                if (response.data.status === "success") {
+                  
+                    alert("Login Successful");
+                   
+                    
+                    navigate("/userdetails");
+                       
+            }
+           
+            else {
+               alert("Email or Password not found, Please Register");
+               navigate("/register")
+           }
+       })
 }
 
-export default Login
+return <div>
+           
+             Email:
+        <Form.Control type="email" placeholder="Enter Email " value={loginDetails.email} onChange={(e) => setLoginDetails({ ...loginDetails, email: e.target.value })} />
+
+        Password:
+
+        <Form.Control type="password" placeholder="Enter Password" value={loginDetails.password} onChange={(e) => setLoginDetails({ ...loginDetails, password: e.target.value })} />
+
+    <Button variant="success" onClick={submit}>Login</Button>
+
+
+    </div>
+    }
+export default Login;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// import registerReducer from  '../Redux/Slices/register';
+import loginReducer from '../Redux/Slices/login';
 export default configureStore({
-  reducer: {},
+  reducer: {
+    login: loginReducer,
+   
+  },
 })
