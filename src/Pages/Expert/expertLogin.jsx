d95451f37e38fae52e62a0766cc0b348eb5c8e47
josphin89 <josejosphin89@gmail.com>
2025-03-18 11:37:37 +0530

    expert log

--- src/Pages/Expert/expertLogin.jsx
+++ src/Pages/Expert/expertLogin.jsx
@@ -1,8 +1,56 @@
-function ExpertLogin(){
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+
+
+function ExpertLogin() {
+
+    const [userExpert,setUserExpert]=useState({
+        email:"",
+        password:""
+    })
+
+    const submit=()=>{
+        if (userExpert.email === "" || userExpert.password === "") {
+            alert("Please Fill");
+            return;
+        }
+        console.log(userExpert)
+        const formData = new FormData();
+        formData.append("email", userExpert.email);
+        formData.append("password", userExpert.password);
+
+        axios.post("https://agaram.academy/api/b4/action.php?request=ai_finance_expert_login", formData)
+            .then((response) => {
+                
+                console.log(response);
+
+                if (response.data.status === "success") {    
+                    alert("Login Successful");    
+            }
+            
+       })
+
+    }
 
 
     return <div>
-        <h2>Welcome to Admin Login</h2>
+        <h2 style={{textAlign:"center"}}>Welcome to expert Login</h2>
+        <div style={{border:"10px solid burlywood" , borderRadius:"15px", width:"500px", padding:"25px", marginLeft:"530px", marginTop:"90px"}}>
+        <Form>
+            <Form.Group className="mb-3">
+                <Form.Label>Email </Form.Label>
+                <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setUserExpert({...userExpert,email:e.target.value})} />
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter password" onChange={(e)=>setUserExpert({...userExpert,password:e.target.value})} />
+            </Form.Group>
+            <Button type="button" style={{marginLeft:"160px"}} onClick={submit}> submit </Button>
+        </Form>
+        </div>
     </div>
 }
 
--- src/Pages/Expert/viewUserDetails.jsx
+++ src/Pages/Expert/viewUserDetails.jsx
@@ -1,11 +1,88 @@
+import { useState, useEffect, split } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {  Row, Col } from 'react-bootstrap';
+import {  useDispatch } from 'react-redux'; ``
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+
+function ViewUserDetails() {
 
 
-function ViewUserDetails(){
+  const [inputValue, setInputValue] = useState({
+    Name: "",
+    Email: "",
+    DoB: "",
+    currentWorkingCompany: "",
+    salary: "",
+    monthlyExpense: "",
+    monthlySavings: "",
+    skills: [],
+    goals: []
+  })
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
 
-    return <div>
-        <h2>ViewUserDetails </h2>
+  const getApi = () => {
+    axios.get('https://agaram.academy/api/b4/action.php?request=ai_finance_view_expert_users&expert_id=1')
+      .then((res) => {
+
+        const getData = res.data.data;
+        setInputValue(getData);
+        console.log(getData)
+
+      })
+  }
+
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+
+  return <div>
+    <h5 style={{ textAlign: "center" }}>ViewUserDetails</h5>
+
+    <div>
+      <Row>
+        <Col sm="10">
+          <div style={{ minwidth: "100%", height: "600px" }}>
+            <table style={{ borderCollapse: "collapse", marginLeft: "90px", minWidth: "100%", borderRadius: "5px 5px 0 0", overflow: "hidden", boxShadow: "0 0 20px black", marginTop: "50px" ,border:"1px solid burlywood"}}>
+              <thead style={{ backgroundColor: "burlywood", color: "white", textAlign: "center", fontWeight: "bold", padding: "15px" }}>
+                <tr>
+                  <th style={{ padding: "10px" }}>Name</th>
+                  <th>Email</th>
+                  <th>DOB</th>
+                  <th>Current Working Company</th>
+                  <th>salary</th>
+                  <th>Monthly Expense</th>
+                  <th>Monthly Savings</th>
+                  <th>Skills</th>
+                  <th>Goals</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style={{ padding: "10px" }}>{inputValue.Name}</td>
+                  <td>{inputValue.Email}</td>
+                  <td>{inputValue.DoB}</td>
+                  <td>{inputValue.currentWorkingCompany}</td>
+                  <td>{inputValue.salary}</td>
+                  <td>{inputValue.monthlyExpense}</td>
+                  <td>{inputValue.monthlySavings}</td>
+                  <td>{inputValue.skills?.map((value) => <li>{value}</li>)}</td>
+                  <td>{inputValue.goals?.map((v) => <li>{v}</li>)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </Col>
+      </Row>
     </div>
+  </div>
 }
 
 export default ViewUserDetails
+
+
+
