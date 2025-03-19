6180c99c1429dd2d7b2f4c27a9ead6cfa91f9d85
Rithin-IT <rithinanto@gmail.com>
2025-03-19 13:05:19 +0530

    process

--- src/Redux/Slices/userDetails.js
+++ src/Redux/Slices/userDetails.js
@@ -13,7 +13,6 @@ export const userDetailsSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
 export const { setUserlist } = userDetailsSlice.actions
 
 export default userDetailsSlice.reducer
